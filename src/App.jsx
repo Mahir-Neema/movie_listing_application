6d6236799fd691a5b2c53bd4f3b6a189628c2f42
Mahir-Neema <mohi10503@gmail.com>
2023-05-21 23:23:01 +0530

    added Loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import { FaMoon,FaSun } from "react-icons/fa";
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { FaMoon, FaSun } from "react-icons/fa";
+import "./App.css";
+import { MagnifyingGlass } from "react-loader-spinner";
 
-const apikey = 'b19d6809'; 
+const apikey = "b19d6809";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('avengers');
+  const [searchTerm, setSearchTerm] = useState("avengers");
   const [favorites, setFavorites] = useState([]);
-  const [theme, setTheme] = useState('dark');
+  const [theme, setTheme] = useState("dark");
+  const [Loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     const timer = setTimeout(() => {
       fetchMovies();
+      setLoading(false);
     }, 2000);
 
     return () => clearTimeout(timer);
@@ -26,7 +30,7 @@ const App = () => {
       const data = await response.json();
       setMovies(data.Search);
     } catch (error) {
-      console.log('Error fetching movies:', error);
+      console.log("Error fetching movies:", error);
     }
   };
 
@@ -45,7 +49,7 @@ const App = () => {
   };
 
   const handleThemeChange = () => {
-    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
   };
 
   return (
@@ -54,7 +58,7 @@ const App = () => {
 
       <div className="theme-change-container">
         <button className="theme-change-button" onClick={handleThemeChange}>
-          {theme === 'light' ? <FaMoon/>:<FaSun/>}
+          {theme === "light" ? <FaMoon /> : <FaSun />}
         </button>
       </div>
 
@@ -67,8 +71,21 @@ const App = () => {
         />
       </div>
 
-      <div className="movies-container">
-        <h2 className='center'>All Movies</h2>
+      {Loading && (
+        <MagnifyingGlass
+          visible={true}
+          height="80"
+          width="80"
+          ariaLabel="MagnifyingGlass-loading"
+          wrapperStyle={{}}
+          wrapperClass="MagnifyingGlass-wrapper"
+          glassColor="#c0efff"
+          color="#e15b64"
+        /> 
+      )}
+
+      {!Loading && <div className="movies-container">
+        <h2 className="center">All Movies</h2>
         {movies &&
           movies.map((movie) => (
             <div key={movie.imdbID} className="movie-item">
@@ -82,14 +99,17 @@ const App = () => {
               </div>
             </div>
           ))}
-      </div>
+      </div>}
 
       <div className="favorites-container">
-        <h2 className='center'>Favorites</h2>
-        
+        <h2 className="center">Favorites</h2>
+
         {favorites.map((movie) => (
           <div key={movie.imdbID} className="movie-item">
-            <button className="star" onClick={() => removeFromFavorites(movie.imdbID)}>
+            <button
+              className="star"
+              onClick={() => removeFromFavorites(movie.imdbID)}
+            >
               ⛔
             </button>
             <img src={movie.Poster} alt={movie.Title} />
